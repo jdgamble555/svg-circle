2ad55c92eac3de0b4428c092b8a0d7af5f0ba322
Jonathan Gamble <jdgamble555@gmail.com>
2025-04-05 12:12:32 -0500

    progress selector

---
+++ components/progress-selector.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const progress = useProgress();
+</script>
+
+<template>
+  <div class="flex items-center justify-center text-center mt-10">
+    <svg-circle :progress>
+      <h1 class="text-2xl font-bold">{{ progress }}%</h1>
+    </svg-circle>
+  </div>
+  <range-input />
+</template>
